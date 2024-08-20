963e8124b10b5145cb0e1a4b25af0b01dbbe7f90
Jonatan Le Douaron <jle-doua@paul-f4Ar1s4.clusters.42paris.fr>
2024-08-20 17:34:12 +0200

    algo prediction

--- algo/undred_num.c
+++ algo/undred_num.c
@@ -6,31 +6,55 @@
 /*   By: jle-doua <jle-doua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 16:42:39 by jle-doua          #+#    #+#             */
-/*   Updated: 2024/08/19 18:11:12 by jle-doua         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:29:30 by jle-doua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// int	get_prev_score(int *lst, int start, int nb, t_info *info)
-// {
-// 	int	i;
-// 	int	index;
+int	get_prev_prevision(int *lst, int start, int nb, t_info *info)
+{
+	int	i;
+	int	index;
+	int	x;
 
-// 	i = start + 1;
-// 	index = ft_get_lower_i(lst, info->lst_a_count);
-// 	while (i % info->lst_a_count != index)
-// 	{
-// 		if (lst[i % info->lst_a_count] < nb && lst[i
-// 			% info->lst_a_count] > lst[index])
-// 		{
-// 			index = i % info->lst_a_count;
-// 		}
-// 		printf("test\n");
-// 		i++;
-// 	}
-// 	return (index);
-// }
+	i = start + 1;
+	index = ft_get_lower_i(lst, info->lst_a_count);
+	while (i % info->lst_a_count != start)
+	{
+		x = ' ';
+		if (lst[i % info->lst_a_count] < nb && lst[i
+			% info->lst_a_count] > lst[index])
+		{
+			index = i % info->lst_a_count;
+		}
+		i++;
+	}
+	return (index);
+}
+
+int	get_next_prevision(int *lst, int start, int nb, t_info *info)
+{
+	int	i;
+	int	index;
+	int	x;
+
+	i = start + 1;
+	index = ft_get_upper_i(lst, info->lst_a_count);
+	while (i % info->lst_a_count != start)
+	{
+		x = ' ';
+		if (lst[i % info->lst_a_count] > nb && lst[i
+			% info->lst_a_count] < lst[index])
+		{
+			x = 'X';
+			index = i % info->lst_a_count;
+		}
+		i++;
+	}
+	return (index);
+}
+
 int	get_next_number_i(int *lst, int start, int nb, t_info *info)
 {
 	int	i;
@@ -44,7 +68,6 @@ int	get_next_number_i(int *lst, int start, int nb, t_info *info)
 			% info->lst_a_count] < lst[index])
 		{
 			index = i % info->lst_a_count;
-			// printf("%s%i%s\n", GREEN, info->lst_a[index], NC);
 		}
 		i++;
 	}
@@ -58,14 +81,12 @@ int	get_prev_number_i(int *lst, int start, int nb, t_info *info)
 
 	i = start;
 	index = ft_get_lower_i(lst, info->lst_a_count);
-	printf("%s%i%s\n", RED, info->lst_a[index], NC);
 	while (i != info->lst_a_count)
 	{
 		if (lst[i % info->lst_a_count] <= nb && lst[i
 			% info->lst_a_count] > lst[index])
 		{
 			index = i % info->lst_a_count;
-			// printf("%s%i%s\n", GREEN, info->lst_a[index], NC);
 		}
 		i++;
 	}
@@ -75,47 +96,85 @@ int	get_prev_number_i(int *lst, int start, int nb, t_info *info)
 typedef struct s_move
 {
 	char	type;
-	int		nb_coup;
+	int		b_l;
+	int		b_u;
 }			t_move;
+
+
 
 t_move	get_worth_move(t_info *info)
 {
 	int		lower_index;
+	int		lower_sec;
 	int		upper_index;
+	int		upper_sec;
 	t_move	best;
 
 	lower_index = get_prev_number_i(info->lst_a, 0, info->lst_b[0], info);
+	lower_sec = get_prev_prevision(info->lst_a, lower_index,
+			info->lst_a[lower_index], info);
 	upper_index = get_next_number_i(info->lst_a, 0, info->lst_b[0], info);
-	if (lower_index < upper_index)
-	{
+	upper_sec = get_next_prevision(info->lst_a, upper_index,
+			info->lst_a[upper_index], info);
+	best.b_l = (lower_sec - lower_index) + lower_index;
+	best.b_u = (upper_sec - upper_index) + upper_index;
+	if (best.b_l < 0)
+		best.b_l *= -1;
+	if (best.b_u < 0)
+		best.b_u *= -1;
+	if (best.b_l < best.b_u || info->lst_b[0] == info->upper)
 		best.type = 'l';
-		best.nb_coup = lower_index;
-	}
 	else
-	{
 		best.type = 'u';
-		best.nb_coup = upper_index;
-	}
 	return (best);
 }
 
 void	undred_num(t_info *info)
 {
-	int	i;
-	t_move best;
+	int		y;
+	int		z;
+	int		i;
+	int		nb_tour;
+	int		nb_r;
+	int		reverse;
+	t_move	best;
 
+	nb_tour = 2;
 	ft_push_b(info);
+	y = 0;
 	while (info->lst_a_count != 0)
 	{
+		if (info->lst_a_count == 1)
+			nb_tour = 1;
 		best = get_worth_move(info);
-		i = -1;
-		while (++i < best.nb_coup)
-			ft_rotate_a(info, 'a');
-		ft_push_b(info);
-		if (best.type == 'l')
-			ft_rotate_b(info, 'a');
+		i = 0;
+		while (i < nb_tour)
+		{
+			reverse = 0;
+			if (best.type == 'l')
+				nb_r = get_prev_number_i(info->lst_a, 0, info->lst_b[0], info);
+			else
+				nb_r = get_next_number_i(info->lst_a, 0, info->lst_b[0], info);
+			z = 0;
+			if (nb_r > info->lst_a_count / 2)
+			{
+				nb_r =  info->lst_a_count - nb_r;
+				reverse = 1;
+			}
+			while (z < nb_r)
+			{
+				if (!reverse)
+					ft_rotate_a(info, 'a');
+				else
+					ft_rrotate_a(info, 'a');
+				z++;
+			}
+			ft_push_b(info);
+			if (best.type == 'l')
+				ft_rotate_b(info, 'a');
+			i++;
+		}
+		y++;
 	}
-	while (info->lst_b_count != 0)
-		ft_push_a(info);
 	print_lst(info);
 }
